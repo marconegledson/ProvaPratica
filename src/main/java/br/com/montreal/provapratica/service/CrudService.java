e3dade03f95c92ca2f5d17d2b2a3a080b63b5790
Marcone Almeida <marconegledson@gmail.com>
2017-05-12 07:32:32 -0300

    Comentarios

--- src/main/java/br/com/montreal/provapratica/service/CrudService.java
+++ src/main/java/br/com/montreal/provapratica/service/CrudService.java
@@ -4,8 +4,17 @@ import java.util.List;
 
 public interface CrudService<T> {
 
+	/**
+	 * Faz a pesquisa utilizando o id que representa a classe
+	 * @param id que representa a chave primaria
+	 * @return O objeto especifico
+	 */
 	T findById(Long id);
 	
+	/**
+	 * Busca por todos os registros de uma determinada classe
+	 * @return A lista de todos os objetos 
+	 */
 	List<T> findAll();
 	
 }
